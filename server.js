40ea0b5c5334f8d9c675cf80a0a84e335cec2af7
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-22 15:20:26 -0300

    Adicionando nodemon e Rotas novas

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listragem de autores',
+    '/sobre': 'Info sobre o sistema'
+}   
+
 const server =http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plan'});
-    res.end ('Curso de Node');
+    res.end (rotas[req.url]);
 })
 
 server.listen(port, () => {

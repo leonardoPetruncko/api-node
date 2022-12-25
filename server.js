a060e43217577e4c92115b56a60a7d49c8b33bdc
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-25 03:52:23 -0300

    refatorando códgio com express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de livros',
-    '/autores': 'Listragem de autores',
-    '/sobre': 'Info sobre o sistema'
-}   
+const port = process.env.port || 3000;
 
-const server =http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plan'});
-    res.end (rotas[req.url]);
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`);
 })

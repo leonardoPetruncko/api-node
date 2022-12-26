be131657fe35fba06431348125c8f8b167183501
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-26 03:06:16 -0300

    Método put

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Express from "express";
 
 const app = Express();
 
-app.use(Express.json())
+app.use(Express.json());
 
 const livros = [
     {id: 1, titulo: "Senhor dos Aneis"},
@@ -10,16 +10,31 @@ const livros = [
 ]
 
 app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node')
+    res.status(200).send('Curso de Node');
 })
 
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
 app.post('/livros', (req, res) => {
     livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso')
+    res.status(201).send('Livro foi cadastrado com sucesso');
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo =req.body.titulo;
+    res.json(livros);
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app;

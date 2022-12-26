5b8fe0e72e67da3d1ce020209f1b3e77011f0ddd
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-26 15:01:29 -0300

    metodo put

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,17 +8,42 @@ class LivroController {
         })
     }
 
+    static listarLivroPorId = (req, res) =>{
+        const id = req.params.id;
+
+        livros.findById(id, (err, livros) => {
+            if(err){
+                res.status(400).send({message: `${err.message} - ID do livro nao localizado`})
+            } else {
+                res.status(200).send(livros);
+            }
+        })
+    }
+
     static cadastrarLivro = (req, res) => {
         let livro = new livros(req.body);
 
         livro.save((err) => {
             if(err) {
-                res.status(500).send({message: `${err} - falha ao cadastrar livro.`})
+                res.status(500).send({message: `${err} - falha ao cadastrar livro.`});
             } else {
                 res.status(201).send(livro.toJSON());
             }
         })
     }
+
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id;
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err) {
+                res.status(200).send({message: 'Livro atualizado com sucesso'})
+            } else {
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+
 }
 
 

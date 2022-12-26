f249377c05cd5169ef0088ec15605b82bc80f780
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-26 14:13:15 -0300

    structuring models and controllers

--- src/app.js
+++ src/app.js
@@ -1,56 +1,57 @@
-import Express from "express";
-import db from "./config/dbConnect.js";
-import livros from "./models/livro.js";
+import express from "express";
+import db from "./config/dbConnect.js"
+import livros from "./models/Livro.js"
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
-    console.log('Conexão com o banco feita com sucesso')
+  console.log('conexão com o banco feita com sucesso')
 })
 
-const app = Express();
-app.use(Express.json());
+const app = express();
+
+app.use(express.json())
+
+routes(app)
 
 // const livros = [
-//     {id: 1, titulo: "Senhor dos Aneis"},
-//     {id: 2, titulo: "O Hobbit"}
+//   {id: 1, "titulo": "Senhor dos Aneis"},
+//   {id: 2, "titulo": "O Hobiit"}
 // ]
 
-app.get('/', (req, res) => {
-    res.status(200).send(`Curso de Node`);
-})
-
 app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros)
-    })
+  livros.find((err, livros) => {
+    res.status(200).json(livros)
+  })
 
 })
 
 app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
 })
 
 app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send(`Livro foi cadastrado com sucesso`);
+  livros.push(req.body);
+  res.status(201).send('Livro foi cadastrado com sucesso')
 })
 
 app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
 })
 
 app.delete('/livros/:id', (req, res) => {
-    let { id } = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1)
-    res.send(`Livro ${id} removido com sucesso`);
+  let {id} = req.params;
+  let index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} removido com sucesso`);
 })
 
+
 function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id)
+  return livros.findIndex(livro => livro.id == id)
 }
 
-export default app;
+export default app
